489514868018ffd1dddb5ef89be623418b113afc
mutaician <109066669+mutaician@users.noreply.github.com>
2024-02-28 19:07:02 +0300

    taking user input and outputing it

---
+++ week1/intro-to-python/user_input.py
@@ -0,0 +1,6 @@
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("Enter your location: ")
+
+print(f"Hello {name}! You are {age} years old and you are from {location}.")
